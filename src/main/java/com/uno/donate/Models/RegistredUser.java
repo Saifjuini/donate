6b45d361599b4c4abfb3ab8ba0db0948d96c4370
saif juini <saif.juini2@gmail.com>
2022-10-29 14:31:39 +0100

    Initial commit

---
+++ src/main/java/com/uno/donate/Models/RegistredUser.java
@@ -0,0 +1,15 @@
+package com.uno.donate.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class RegistredUser {
+}
